f2d4ff92b70f674a315055d4166d4c0ef164388a
Matt Ossman <mattossman@gmail.com>
2018-12-18 01:22:18 -0500

    More tests, and allow shallow object conversion back to json

--- test/test_dto.js
+++ test/test_dto.js
@@ -6,6 +6,9 @@ class TestDto extends BaseDto {
 			'_id',
 			'_rev',
 			'number_as_string',
+			'float_as_string',
+			{name: 'boolean_value_true', type: Boolean},
+			{name: 'boolean_value_false', type: Boolean},
 			{name: 'typed_number', type: Number},
 			{name: 'typed_number_float', type: Number},
 			{name: 'nested_dto', type: TestNestedDto},
@@ -26,7 +29,10 @@ describe('BaseDto', () => {
 		let dto
 		let _id = 'test-id'
 		let _rev = 'test-rev'
+		let boolean_value_true = true
+		let boolean_value_false = false
 		let number_as_string = 5 //This should be interpreted as a string
+		let float_as_string = 5.3 //This should be interpreted as a string
 		let typed_number = 5 //This should be interpreted as a Number
 		let typed_number_float = 5.2 //This should be interpreted as a Number as well
 		let nested_dto = {message: 'Hello world!'}
@@ -34,14 +40,30 @@ describe('BaseDto', () => {
 		let nested_dto_dictionary = {'three': {message: 'three'}, 'four': {message: 'four'}}
 
 		it('should be built from a JavaScript Object', () => {
-			dto = new TestDto({_id, _rev, number_as_string, typed_number, typed_number_float,
+			dto = new TestDto({_id, _rev, boolean_value_true, boolean_value_false,
+				number_as_string, float_as_string, typed_number, typed_number_float,
 				nested_dto, nested_dto_array, nested_dto_dictionary})
 
 			expect(dto._id).toEqual(_id)
 			expect(dto._rev).toEqual(_rev)
-			expect(dto.number_as_string).toEqual('5')
+
+			// Test native values, for example we want to make sure we aren't calling "new String", only "String"
+			// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+			expect(typeof dto.boolean_value_true).toEqual('boolean')
+			expect(dto.boolean_value_true).toBe(true)
+			expect(typeof dto.boolean_value_false).toEqual('boolean')
+			expect(dto.boolean_value_false).toBe(false)
+
+			expect(typeof dto.typed_number).toEqual('number')
 			expect(dto.typed_number).toEqual(5)
+			expect(typeof dto.typed_number_float).toEqual('number')
 			expect(dto.typed_number_float).toEqual(5.2)
+
+			expect(typeof dto.number_as_string).toEqual('string')
+			expect(dto.number_as_string).toEqual('5')
+			expect(typeof dto.float_as_string).toEqual('string')
+			expect(dto.float_as_string).toEqual('5.3')
+
 			expect(dto.nested_dto).toEqual(jasmine.any(TestNestedDto))
 			expect(dto.nested_dto.message).toEqual(nested_dto.message)
 			expect(dto.nested_dto_array[0]).toEqual(jasmine.any(TestNestedDto))
@@ -55,7 +77,7 @@ describe('BaseDto', () => {
 		it('should gracefully handle JSON with missing fields', () => {
 			let dto_with_defaults = new TestDto({_id, _rev})
 
-			expect(dto_with_defaults.nested_dto).toEqual(undefined)
+			expect(dto_with_defaults.nested_dto).toEqual(jasmine.any(TestNestedDto))
 			expect(dto_with_defaults.nested_dto_array).toEqual([])
 			expect(dto_with_defaults.nested_dto_dictionary).toEqual({})
 		})
@@ -65,8 +87,16 @@ describe('BaseDto', () => {
 
 			expect(object['_id']).toEqual(_id)
 			expect(object['_rev']).toEqual(_rev)
+
+			expect(object['boolean_value_true']).toEqual(boolean_value_true)
+			expect(object['boolean_value_false']).toEqual(boolean_value_false)
+			expect(object['number_as_string']).toEqual(String(number_as_string))
+			expect(object['float_as_string']).toEqual(String(float_as_string))
+			expect(object['typed_number']).toEqual(typed_number)
+			expect(object['typed_number_float']).toEqual(typed_number_float)
 			expect(object['nested_dto']).toEqual(nested_dto)
 			expect(object['nested_dto_array']).toEqual(nested_dto_array)
+			expect(object['nested_dto_dictionary']).toEqual(nested_dto_dictionary)
 		})
 	})
 })
