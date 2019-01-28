7bf9db343de5d2fa8dc285210d45f04dfac3695f
Matt Ossman <mattossman@gmail.com>
2019-01-27 23:46:09 -0500

    remove unused import

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const BaseDTO = require('./src/base_dto')
 const Client = require('./src/client')
 const DatabaseAPI = require('./src/database_api')
-const DocumentAPI = require('./src/document_api')
 
 module.exports = {
 	BaseDTO,
 	Client,
-	DatabaseAPI,
-	DocumentAPI
+	DatabaseAPI
 }
